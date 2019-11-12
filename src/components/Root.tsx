ce5f5255e697175487f5af1111944915e710538b
hasparus <hasparus@gmail.com>
2019-11-12 21:21:57 +0100

    Add background color to theme-color meta tag

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,20 +1,30 @@
 /** @jsx jsx */
-import { jsx, Styled as s } from "theme-ui";
-import React from "react";
+import { jsx, Styled as s, useThemeUI } from "theme-ui";
+import React, { Fragment } from "react";
+import { Helmet } from "react-helmet";
 
 export const Root = (
   props: Omit<React.ComponentProps<typeof s.root>, "ref">
-) => (
-  <s.root
-    sx={{
-      maxWidth: "80ch",
-      mx: "auto",
-      mt: 3,
-      mb: 6,
-      "*": {
-        boxSizing: "border-box",
-      },
-    }}
-    {...props}
-  />
-);
+) => {
+  const { theme } = useThemeUI();
+  return (
+    <Fragment>
+      <Helmet>
+        <meta name="theme-color" content={theme.colors!.background} />
+      </Helmet>
+      <s.root
+        sx={{
+          // this is about "80ch" but I'd like to avoid layout jumping on font load,
+          maxWidth: "824px",
+          mx: "auto",
+          mt: 3,
+          mb: 6,
+          "*": {
+            boxSizing: "border-box",
+          },
+        }}
+        {...props}
+      />
+    </Fragment>
+  );
+};
