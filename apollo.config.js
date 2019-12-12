20c5fbcbd6b877d81c6b13cabb802180099362d8
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2019-12-12 09:05:17 +0100

    Revert "Draft Deliver post"
    This reverts commit 133e2b9630be0fe74b37edbfc6a0860f0632ae9e.

--- apollo.config.js
+++ apollo.config.js
@@ -5,7 +5,7 @@ module.exports = {
     includes: ["./src/**/*.tsx","./src/**/*.ts","./node_modules/gatsby-source-contentful/src/*.js","./node_modules/gatsby-transformer-sharp/src/*.js","./node_modules/gatsby-image/src/*.js"],
     service: {
       name: "gatsbySchema",
-      localSchemaFile: "gql-schema.json"
+      localSchemaFile: "./gql-schema.json"
     },
     tagName: "graphql"
   }
