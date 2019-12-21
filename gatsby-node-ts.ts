332906c1bdff8cd7a2f1ce1e11c0dc0f8457d013
hasparus <hasparus@gmail.com>
2019-12-21 23:35:03 +0100

    Run puppeteer in headless mode

--- gatsby-node-ts.ts
+++ gatsby-node-ts.ts
@@ -193,7 +193,7 @@ let browser: Browser;
 export async function onPreInit() {
   browser = await puppeteer.launch({
     // Toggle to preview generated images
-    headless: false,
+    headless: true,
   });
 }
 
