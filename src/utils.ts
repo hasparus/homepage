74deabe19d64a7e8c992345246ab932295e9e96d
hasparus <hasparus@gmail.com>
2019-12-27 11:48:24 +0100

    Handle rgb color strings in isColorDark

--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,48 @@
 import { AssertionError } from "assert";
+import { flow } from "fp-ts/lib/function";
+
+interface RGB {
+  r: number;
+  g: number;
+  b: number;
+}
+
+function RGB(color: string): RGB {
+  let r: number;
+  let g: number;
+  let b: number;
+  if (color.startsWith("rgb")) {
+    [r, g, b] = color
+      .match(/rgba?\((\d+),\s*(\d+),\s*(\d+)/)!
+      .slice(1, 4)
+      .map(Number);
+  } else if (color.startsWith("#")) {
+    const col =
+      color.length === 7
+        ? color.slice(1)
+        : color
+            .match(/#(\w)(\w)(\w)/)!
+            .slice(1, 4)
+            .map(ch => ch + ch)
+            .join("");
+    [r, g, b] = col
+      .match(/(\w\w)(\w\w)(\w\w)/)!
+      .slice(1, 4)
+      .map(x => Number(`0x${x}`));
+  } else {
+    throw new Error(`color ${color} not handled`);
+  }
+
+  return { r, g, b } as RGB;
+}
 
 export const isString = (x: unknown): x is string => typeof x === "string";
 
 /**
- * @param color 6 or 3 letter hex
  * @example
  * contrastingTextColor("#fff") === "black";
  */
-
-export const isColorDark = (color: string) => {
-  const col =
-    color.length === 7
-      ? color.slice(1)
-      : color
-          .match(/#(\w)(\w)(\w)/)!
-          .slice(1, 4)
-          .map(ch => ch + ch)
-          .join("");
-
-  const [r, g, b] = col
-    .match(/(\w\w)(\w\w)(\w\w)/)!
-    .slice(1, 4)
-    .map(x => Number(`0x${x}`));
-
+export const isRGBDark = ({ r, g, b }: RGB) => {
   /**
    * @see http://alienryderflex.com/hsp.html
    * @see https://medium.com/the-mvp/finally-a-definitive-way-to-make-gradients-beautiful-6b27af88f5f
@@ -31,6 +51,8 @@ export const isColorDark = (color: string) => {
 
   return hsp <= 125;
 };
+
+export const isColorDark = flow(RGB, isRGBDark);
 
 export const contrastingTextColor = (color: string) => {
   return isColorDark(color) ? "white" : "black";
