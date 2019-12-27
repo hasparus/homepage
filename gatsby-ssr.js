19378749952b98e315e48575acba0b15dba53ce0
hasparus <hasparus@gmail.com>
2019-12-27 11:51:29 +0100

    Fix gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-unresolved
-import { pageCtx } from "./src/components/pageCtx";
+const { pageCtx } = require("./src/components/pageCtx");
 
-export const { wrapPageElement } = pageCtx;
+exports.wrapPageElement = pageCtx.wrapPageElement;
