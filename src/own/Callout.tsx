fb832457d5d30344021d34dfcb0fd03ab1fc6f08
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-01-29 09:36:44 -0300

    Rename BoxedText to Callout and accept icon prop

---
+++ src/own/Callout.tsx
@@ -0,0 +1,21 @@
+import { JSX, splitProps } from "solid-js";
+
+export interface CalloutProps extends JSX.HTMLAttributes<HTMLDivElement> {
+  icon?: string;
+}
+
+export function Callout(props: CalloutProps) {
+  const [own, rest] = splitProps(props, ["icon"]);
+
+  return (
+    <div
+      classList={{
+        "bg-gray-100 dark:bg-gray-800/60 rounded py-2 px-3 text-sm relative":
+          true,
+        [`relative after:content-['${own.icon}'] after:absolute after:top-2 after:right-3 after:text-base`]:
+          !!own.icon,
+      }}
+      {...rest}
+    />
+  );
+}
