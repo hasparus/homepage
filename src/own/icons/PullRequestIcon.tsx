21f90ac5484cb5a11c55b07f6552b674d31fc740
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-01-31 11:41:36 -0300

    Add PullRequestIcon

---
+++ src/own/icons/PullRequestIcon.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "solid-js";
+
+import pullRequestSvg from "./git-pull-request.svg";
+
+export function PullRequestIcon(
+  props: JSX.ImgHTMLAttributes<HTMLImageElement>
+) {
+  return <img src={pullRequestSvg} alt="" {...props} />;
+}
