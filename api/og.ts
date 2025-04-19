60edd91f59a6b3d8c1d26b8679b355256ef46488
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-04-19 19:18:51 +0200

    Fix my messy mistake

--- api/og.ts
+++ api/og.ts
@@ -43,7 +43,7 @@ export default async function og(req: Request) {
       postImage.startsWith("/") &&
       !postImage.startsWith("/content/")
     ) {
-      postImage = await getDataURI();
+      postImage = await getDataURI(postImage);
     }
 
     return new ImageResponse(
