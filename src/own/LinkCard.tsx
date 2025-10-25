302509c4dd6c9c877ad1cbc94e4ffedd9c23e27e
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-10-25 23:42:22 +0300

    Make LinkCard accept children

--- src/own/LinkCard.tsx
+++ src/own/LinkCard.tsx
@@ -2,20 +2,15 @@ import type { JSX } from "solid-js";
 
 import { ExternalLinkIcon } from "./icons/ExternalLinkIcon";
 
-export function LinkCard(props: JSX.IntrinsicElements["a"]) {
+export interface LinkCardProps extends JSX.HTMLAttributes<HTMLAnchorElement> {}
+
+export function LinkCard(props: LinkCardProps) {
   return (
     <a
-      href="https://matryoshka-code.vercel.app/"
       class="hover:translate--px group relative flex w-fit flex-col items-center gap-2 overflow-hidden rounded-sm border border-gray-200 bg-gray-50 pb-2 shadow-sm transition duration-75 ease-out hover:shadow-none dark:border-gray-700 dark:bg-gray-800"
       {...props}
     >
-      <img
-        width={280}
-        class="dark:opacity-90"
-        alt="matryoshkas"
-        src="https://images-na.ssl-images-amazon.com/images/I/5186qD8N%2BpL._SX466_.jpg"
-      />
-      <span>https://matryoshka-code.now.sh</span>
+      {props.children}
       <ExternalLinkIcon class="absolute right-2 top-2 z-10 size-4 opacity-60" />
     </a>
   );
